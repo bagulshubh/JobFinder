cc47203a9521993277ed8f15c405d69543feec2a
bagulshubh <bagulshubham72@gmail.com>
2023-11-16 19:47:13 +0530

    Changes

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://jobfindeer.netlify.app/",
+        origin:"https://jobfindeer.netlify.app",
  		credentials:true,
     })
 )
